dcaa7157af6cc4bf4fb9002fd1dad09c430c724d
hipfjohnyi <yi.whatsup@gmail.com>
2017-07-12 16:20:20 +0800

    change crlf to lf

--- 1.sql
+++ 1.sql
@@ -1,9 +1,9 @@
- CREATE TABLE `virtual_domains` (
+CREATE TABLE `virtual_domains` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
- CREATE TABLE `virtual_users` (
+  CREATE TABLE `virtual_users` (
   `id` int(11) NOT NULL auto_increment,
   `domain_id` int(11) NOT NULL,
   `password` varchar(106) NOT NULL,
@@ -12,7 +12,7 @@
   UNIQUE KEY `email` (`email`),
   FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-   CREATE TABLE `virtual_aliases` (
+CREATE TABLE `virtual_aliases` (
   `id` int(11) NOT NULL auto_increment,
   `domain_id` int(11) NOT NULL,
   `source` varchar(100) NOT NULL,
